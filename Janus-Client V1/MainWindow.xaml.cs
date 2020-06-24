53a84ea15970e69a2f56eaaf247b84630f4e6a55
edvbl <edvbl@DESKTOP-GRPF9D4>
2020-06-24 20:28:06 +0200

    Projektdateien hinzufügen.

---
+++ Janus-Client V1/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Janus_Client_V1
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DeployCupCakes(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+        }
+
+        private void LaunchGitHubSite(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+        }
+    }
+}
