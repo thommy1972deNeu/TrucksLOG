87be0ac7a77adf77d1857eef33eec1725f63f70f
edvbl <edvbl@DESKTOP-GRPF9D4>
2020-06-25 16:57:49 +0200

    Einige Änderungen am Design

---
+++ Janus-Client V1/Klassen/Messages.cs
@@ -0,0 +1,20 @@
+﻿using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Janus_Client_V1.Klassen
+{
+    public class Messages
+    {
+        public async void Schreiben(string titel, string text)
+        {
+            var metroWindow = (Application.Current.MainWindow as MetroWindow);
+            await metroWindow.ShowMessageAsync(titel, text);
+        }
+    }
+}
--- Janus-Client V1/MainWindow.xaml.cs
+++ Janus-Client V1/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using Janus_Client_V1.Klassen;
+using System.Windows;
+
 namespace Janus_Client_V1
 {
     /// <summary>
@@ -6,17 +8,47 @@ namespace Janus_Client_V1
     /// </summary>
     public partial class MainWindow
     {
+        Messages msg = new Messages();
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void DeployCupCakes(object sender, System.Windows.RoutedEventArgs e)
+
+
+
+        private void LaunchGitHubSite(object sender, System.Windows.RoutedEventArgs e)
+        {
+            msg.Schreiben("Fehler", "Diese Funktion wird bald eingebaut...");
+        }
+
+        private void Beta_Tester(object sender, RoutedEventArgs e)
+        {
+            msg.Schreiben("Fehler", "Diese Funktion wird bald eingebaut...");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
-        private void LaunchGitHubSite(object sender, System.Windows.RoutedEventArgs e)
+        private void Spende_einen_Kaffee(object sender, RoutedEventArgs e)
+        {
+            msg.Schreiben("Fehler", "Diese Funktion wird bald eingebaut...");
+        }
+
+        private void MainWindow_Oeffnen(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Seite_Kontakt_oeffnen(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Hauptmenu_Click(object sender, RoutedEventArgs e)
         {
 
         }
