2964156182c8ebc375b1e1dc65451f5b890c0527
Thommy <info@zwpc.de>
2020-06-29 04:22:20 +0200

    Tankpreise je Land - Klasse eingefügt

---
+++ Janus-Client V1/Klassen/tankpreise.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Janus_Client_V1.Klassen
+{
+    public class Tankpreise
+    {
+        public double Belgien = 1.24;
+        public double Tschechien = 1.24;
+        public double Dänemark = 1.24;
+        public double Deutschland = 1.24;
+        public double Frankreich = 1.24;
+        public double Großbritannien = 1.24;
+        public double Italien = 1.24;
+        public double Luxemburg = 1.24;
+        public double Niederlande = 1.24;
+        public double Norwegen = 1.24;
+        public double Österreich = 1.24;
+        public double Polen = 1.24;
+        public double Slowakai = 1.24;
+        public double Schweden = 1.24;
+        public double Schweiz = 1.24;
+        public double Ungarn = 1.24;
+        public double Bulgarien = 1.24;
+        public double Estland = 1.24;
+        public double Finnland = 1.24;
+        public double Lettland = 1.24;
+        public double Litauen = 1.24;
+        public double Rumänien = 1.24;
+        public double Russland = 1.24;
+        public double Türkei = 1.24;
+
+        public double Arizona = 1.24;
+        public double Kalifornien = 1.24;
+        public double Nevada = 1.24;
+        public double New_Mexiko = 1.24;
+        public double Oregon = 1.24;
+        public double Utah = 1.24;
+        public double Washington = 1.24;
+
+
+
+    }
+}
